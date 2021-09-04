acd34f770ebbc7c80eedd26df4b9216b554acc36
suadtalafha <suadhusam@yahoo.com>
2021-09-04 22:41:50 +0300

    save

---
+++ javascript/Code-challange15/binaryTreeSearch.js
@@ -0,0 +1,60 @@
+"use strict";
+
+const Node = require("./node");
+const BinaryTree = require("./binaryTree");
+
+class BinaryTreeSearch extends BinaryTree {
+  constructor(root) {
+    super(root);
+  }
+
+  contains(value) {
+    
+
+    let result = false;
+
+    let _contains = (node) => {
+      if (node.value == value) result = true;
+      if (node.left) _contains(node.left);
+      if (node.right) _contains(node.right);
+    };
+
+    _contains(this.root, value);
+    return result;
+  }
+
+  add(value) {
+    let node = new Node(value);
+    if (!this.root) {
+      this.root = node;
+    } else {
+      let tempNode = this.root;
+      let _check = (value) => {
+        let node = new Node(value);
+        try {
+          if (node.value > tempNode.value) {
+            if (tempNode.right) {
+              tempNode = tempNode.right;
+              _check(node);
+            } else {
+              tempNode.right = node;
+            }
+          }
+          if (node.value < tempNode.value) {
+            if (tempNode.left) {
+              tempNode = tempNode.left;
+              _check(node);
+            } else {
+              tempNode.left = node;
+            }
+          }
+        } catch {
+          return "some thing went wrong";
+        }
+      };
+      _check(value);
+    }
+  }
+}
+
+module.exports = BinaryTreeSearch;
